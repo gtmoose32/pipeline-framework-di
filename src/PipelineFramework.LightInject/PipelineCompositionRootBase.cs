3ce59a85abafe4e3c468a3d430b3db57f43df39e
Marty Mathis <Marty.Mathis@assurant.com>
2019-01-20 10:34:49 -0500

    DI projects moved from pipeline-framework repo to own repo.

---
+++ src/PipelineFramework.LightInject/PipelineCompositionRootBase.cs
@@ -0,0 +1,16 @@
+﻿using LightInject;
+using PipelineFramework.Abstractions;
+
+namespace PipelineFramework.LightInject
+{
+    public abstract class PipelineCompositionRootBase : ICompositionRoot
+    {
+        public virtual void Compose(IServiceRegistry registry)
+        {
+            var container = registry as IServiceFactory;
+
+            registry.Register<IPipelineComponentResolver>(factory =>
+                new PipelineComponentResolver(container), new PerContainerLifetime());
+        }
+    }
+}
