479e9a3357dab51e6ef7e62458c15d0e8827214b
Marty Mathis <Marty.Mathis@assurant.com>
2019-01-21 14:30:39 -0500

    Added unit tests for light inject logging code.

---
+++ tests/PipelineFramework.LightInject.Tests/Interception/AsyncPipelineComponentInterceptorTests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using LightInject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using PipelineFramework.Abstractions;
+using PipelineFramework.LightInject.Interception;
+using PipelineFramework.Tests.SharedInfrastructure;
+using Serilog;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PipelineFramework.LightInject.Tests.Interception
+{
+    [TestClass]
+    public class AsyncPipelineComponentInterceptorTests
+    {
+        private IServiceContainer _container;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _container = new ServiceContainer(new ContainerOptions { EnablePropertyInjection = false });
+        }
+
+        [TestMethod]
+        public async Task AsyncInterceptor_Test()
+        {
+            var logger = Substitute.For<ILogger>();
+            _container.RegisterInstance(logger);
+            _container.RegisterFrom<DefaultLoggingCompositionRoot>();
+            _container.Register<IAsyncPipelineComponent<TestPayload>, AsyncTestComponent>();
+
+            var component = _container.GetInstance<IAsyncPipelineComponent<TestPayload>>();
+            var result = await component.ExecuteAsync(new TestPayload(), CancellationToken.None);
+
+            result.Should().NotBeNull();
+            logger.Received().Information(Arg.Is(LoggingMessages.SuccessMessage), Arg.Any<long>());
+        }
+
+        [TestMethod]
+        public void AsyncInterceptor_ComponentException_Test()
+        {
+            var logger = Substitute.For<ILogger>();
+            _container.RegisterInstance(logger);
+            _container.RegisterFrom<DefaultLoggingCompositionRoot>();
+            _container.Register<IAsyncPipelineComponent<TestPayload>, BarExceptionComponent>();
+
+            var component = _container.GetInstance<IAsyncPipelineComponent<TestPayload>>();
+            Func<Task> act = () => component.ExecuteAsync(new TestPayload(), CancellationToken.None);
+
+            act.Should().ThrowExactly<NotImplementedException>();
+            logger.Received().Error(Arg.Any<Exception>(), Arg.Is(LoggingMessages.ExceptionMessage), Arg.Any<long>());
+        }
+    }
+}
---
+++ tests/PipelineFramework.LightInject.Tests/Interception/PipelineComponentInterceptorTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using LightInject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using PipelineFramework.Abstractions;
+using PipelineFramework.LightInject.Interception;
+using PipelineFramework.Tests.SharedInfrastructure;
+using Serilog;
+using System;
+using System.Threading;
+
+namespace PipelineFramework.LightInject.Tests.Interception
+{
+    [TestClass]
+    public class PipelineComponentInterceptorTests
+    {
+        private IServiceContainer _container;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _container = new ServiceContainer(new ContainerOptions { EnablePropertyInjection = false });
+        }
+
+        [TestMethod]
+        public void Interceptor_Test()
+        {
+            var logger = Substitute.For<ILogger>();
+            _container.RegisterInstance(logger);
+            _container.RegisterFrom<DefaultLoggingCompositionRoot>();
+            _container.Register<IPipelineComponent<TestPayload>, FooComponent>();
+
+            var component = _container.GetInstance<IPipelineComponent<TestPayload>>();
+            var result = component.Execute(new TestPayload(), CancellationToken.None);
+
+            result.Should().NotBeNull();
+            logger.Received().Information(Arg.Is(LoggingMessages.SuccessMessage), Arg.Any<long>());
+        }
+
+        [TestMethod]
+        public void Interceptor_ComponentException_Test()
+        {
+            var logger = Substitute.For<ILogger>();
+            _container.RegisterInstance(logger);
+            _container.RegisterFrom<DefaultLoggingCompositionRoot>();
+            _container.Register<IPipelineComponent<TestPayload>, BarExceptionComponent>();
+
+            var component = _container.GetInstance<IPipelineComponent<TestPayload>>();
+            Action act = () => component.Execute(new TestPayload(), CancellationToken.None);
+
+            act.Should().ThrowExactly<NotImplementedException>();
+            logger.Received().Error(Arg.Any<Exception>(), Arg.Is(LoggingMessages.ExceptionMessage), Arg.Any<long>());
+        }
+    }
+}
